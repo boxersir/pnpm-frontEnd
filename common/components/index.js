dad7e54ab3ebc4417a73c4cc1b8bffc7abd19371
caixin <caixin@innovsharing.com>
2023-06-28 17:08:18 +0800

    init

---
+++ common/components/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Date: 2023-06-28 16:06:25
+ * @LastEditTime: 2023-06-28 16:09:21
+ * @FilePath: \pnpmproject\common\components\index.js
+ */
+export * from './waterMark'
---
+++ common/components/waterMark/index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2023-06-28 15:57:31
+ * @LastEditTime: 2023-06-28 16:11:57
+ * @LastEditors: DESKTOP-LTTLG5D
+ * @Description: In User Settings Edit
+ * @FilePath: \pnpmproject\common\components\waterMark\index.vue
+-->
+<template>
+  <div>
+    watermark
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
